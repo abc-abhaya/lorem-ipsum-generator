3376e322062f425e3826f58b43c4379fbf2098b7
abc-abhaya <162488354+abc-abhaya@users.noreply.github.com>
2024-07-06 19:05:58 +0545

    plain html

--- src/scripts/favicon.js
+++
@@ -1,7 +0,0 @@
-export default function setFavicon(url, type = 'x-icon') {
-	const link = document.createElement('link');
-	link.type = 'image/' + type;
-	link.rel = 'shortcut icon';
-	link.href = url;
-	document.head.appendChild(link);
-}
